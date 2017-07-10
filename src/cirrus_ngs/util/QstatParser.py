07b66bb95170debd339920496c6c4a30a6a53348
mustafa-guler <mguler@ucsd.edu>
2017-07-10 12:52:05 -0700

    added tests for check_status of WGSPipelineManger

--- src/cirrus_ngs/util/QstatParser.py
+++ src/cirrus_ngs/util/QstatParser.py
@@ -2,9 +2,10 @@ __author__ = "Mustafa Guler<mguler@ucsd.edu>"
 
 def get_job_ids(qstat):
     qstat_list = qstat.split("\n")
-    qstat_list[:] = qstat_list[2:]
-    job_ids = [[int(qstat_list[x].split()[0]), qstat_list[x].split()[4]]
-        for x in range(len(qstat_list))]
+    qstat_list = qstat_list[2:-1]
+
+    job_ids = [[int(qstat_list[x].split()[0]), qstat_list[x].split()[4]] 
+            for x in range(len(qstat_list))]
     return job_ids
 
 ## parses qstat output to give user status information
@@ -17,16 +18,14 @@ def parse_qstat(job_ids, job_name):
             "d" : 0,
             "E" : 0}
 
-    for line in qstat:
+    for job in job_ids:
         line = line.strip().split()
         if job_name in line[2]:
             if "E" in line[4]:
                 jobs["E"] += 1
             elif "d" in line[4]:
                 jobs["d"] += 1
-            elif "t" in line[4]: #do nothing, catching edge
-                continue
-            else:
+            elif line[4] == "qw" or line[4] == "r":
                 jobs[line[4]] += 1
 
     print("The status of your \"%s\" job:" % job_name)
--- test/test_cirrus_ngs/test_util/test_QstatParser.py
+++ test/test_cirrus_ngs/test_util/test_QstatParser.py
@@ -2,31 +2,50 @@ import unittest
 import sys
 import os
 sys.path.append(os.getcwd().replace("test", "src"))
-import cirrus_ngs.util.QstatParser as test_class
+import cirrus_ngs.util.QstatParser as QstatParser
 
 class Tests(unittest.TestCase):
     def test_get_job_ids(self):
         qstat = """job-ID  prior   name       user         state submit/start at     queue                          slots ja-task-ID
 -----------------------------------------------------------------------------------------------------------------
 362 0.00000 slow.sh    ec2-user     qw    07/07/2017 18:55:45                                    1
-407 0.50000 test.sh    user         r     07/07/2017 18:23:12     queue_here                     1"""
-        output = test_class.get_job_ids(qstat)
+407 0.50000 test.sh    user         r     07/07/2017 18:23:12     queue_here                     1
+"""
+        output = QstatParser.get_job_ids(qstat)
         #tests basic functionality of get_job_ids
         self.assertEqual(output, [[362,"qw"], [407, "r"]])
 
         bad_qstat = """job-ID  prior  name       notright     incorrect
 -------------------------------------------------------------------
-hello"""
+hello
+"""
         #makes sure that the id is an int and the function fails with strings
-        self.assertRaises(ValueError, test_class.get_job_ids, bad_qstat)
+        self.assertRaises(ValueError, QstatParser.get_job_ids, bad_qstat)
 
         bad_qstat = """job-ID  prior  name       notright     incorrect
 -------------------------------------------------------------------
 1 2
-3 ls -l h"""
+3 ls -l h
+"""
 
         #makes sure the function fails with an incorrect qstat input
-        self.assertRaises(IndexError, test_class.get_job_ids, bad_qstat)
+        self.assertRaises(IndexError, QstatParser.get_job_ids, bad_qstat)
+
+    #bug with extra entry in qstat_list not caught with first test
+    #if bug reoccurs it will be caught with following test
+    #extra newline in qstat is what caused prior failure
+    def test_extra_job_id(self):
+        qstat = """job-ID  prior   name       user         state submit/start at     queue                          slots ja-task-ID
+-----------------------------------------------------------------------------------------------------------------
+362 0.00000 slow.sh    ec2-user     qw    07/07/2017 18:55:45                                    1
+407 0.50000 test.sh    user         r     07/07/2017 18:23:12     queue_here                     1
+""" #newline in true qstat, wasn't in test (prev test was edited too)
+        output = QstatParser.get_job_ids(qstat)
+        self.assertEqual(output, [[362,"qw"], [407, "r"]])
+        
+        
+        
+
 
 if __name__ == "__main__":
     unittest.main()
