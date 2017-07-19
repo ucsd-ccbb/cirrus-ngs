765de81573ebab6af1ee0497a0de3d5694875005
Mengyi Liu <mikoliu798@gmail.com>
2017-07-18 17:20:05 -0700

    Added miRNAPipeline notebook
    Added a functional jupyter notebook for miRNA pipeline, with python codes for downloading fastq file and running fastqc, as well as new scripts for these two steps.

--- src/cirrus_ngs/server/SmallRNASeqPipeline/scripts/download.sh
+++ src/cirrus_ngs/server/SmallRNASeqPipeline/scripts/download.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-inputFile=$1
-S3DownloadURL=$2
-dataFolder=$3
+project_name=$1
+file_suffix=$2
+file_path=$3
+file1_name=$4
+file2_name=$5
+input_address=$6
 
-echo "downloading file: "$S3DownloadURL/$inputFile
+echo $file_suffix
+echo $file1_name
+echo $file2_name
 
-if [ ! -f $dataFolder$inputFile ]
-then
-    aws s3 cp $S3DownloadURL/$inputFile $dataFolder/ 
+workspace=$file_path/$project_name
+
+if [ ! -d $workspace/$file1_name ]; then
+   mkdir -p $workspace/$file1_name
 fi
+
+# redirecting all output to a file
+exec 1>>$workspace/'download.log'
+exec 2>>$workspace/'download.log'
+
+echo "Downloading $file1_name ..."
+
+if [ $file2_name == "NULL" ]; then
+    aws s3 cp $input_address/$file1_name$file_suffix".gz" $workspace/$file1_name/
+    gunzip $workspace/$file1_name/$file1_name$file_suffix".gz"
+else
+    aws s3 cp $input_address/$file1_name$file_suffix".gz" $workspace/$file1_name/
+    gunzip $workspace/$file1_name/$file1_name$file_suffix".gz"
+    aws s3 cp $input_address/$file2_name$file_suffix".gz" $workspace/$file1_name/
+    gunzip $workspace/$file1_name/$file2_name$file_suffix".gz"
+fi
+
