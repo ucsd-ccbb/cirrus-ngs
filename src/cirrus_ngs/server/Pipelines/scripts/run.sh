ee46170a21f52e53188d9997a5eec0808bc1fc1c
Mengyi Liu <mikoliu798@gmail.com>
2017-08-04 16:14:00 -0700

    Added MultiQC notebook
    multiqc notebook with python code and shell script, still under construction

--- src/cirrus_ngs/server/Pipelines/scripts/run.sh
+++ src/cirrus_ngs/server/Pipelines/scripts/run.sh
@@ -4,9 +4,13 @@ yaml_file=$1
 log_dir=$2
 pipeline=$3     #specific pipeline file
 
-mkdir -p $log_dir
+echo $yaml_file
+echo $log_dir
+echo $pipeline
 
-exec 1>>$log_dir/run.log
-exec 2>>$log_dir/run.log
+mkdir -p $log_dir
+log_file=$log_dir/'run.log'
+exec 1>>$log_file
+exec 2>>$log_file
 
 /shared/workspace/software/anaconda3/bin/python /shared/workspace/Pipelines/$pipeline $yaml_file
