be85d8be50b61cf1239acf7d7106519c108aae3f
Mengyi Liu <mikoliu798@gmail.com>
2017-08-30 16:32:39 -0700

    modified shell scripts

--- src/cirrus_ngs/server/Pipelines/scripts/RNA/k_align.sh
+++ src/cirrus_ngs/server/Pipelines/scripts/RNA/k_align.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# This shell script uses kallisto alignment.
 
 project_name=$1
-file_suffix=$2  #extension of input file
+file_suffix=$2  #extension of input file, does not include .gz if present in input
 root_dir=$3
 fastq_end1=$4
 fastq_end2=$5
@@ -9,18 +10,16 @@ input_address=$6    #this is an s3 address e.g. s3://path/to/input/directory
 output_address=$7   #this is an s3 address e.g. s3://path/to/output/directory
 log_dir=$8
 is_zipped=$9    #either "True" or "False", indicates whether input is gzipped
+num_threads=${10}
 
 #logging
-log_dir=$log_dir/$fastq_end1
-mkdir -p $log_dir
-log_file=$log_dir/'bowtie2.log'
+mkdir -p $log_dir/$fastq_end1
+log_file=$log_dir/$fastq_end1/'k_align.log'
 exec 1>>$log_file
 exec 2>>$log_file
 
 #prepare output directories
 workspace=$root_dir/$project_name/$fastq_end1
-basename=hairpin_human
-
 mkdir -p $workspace
 
 ##DOWNLOAD##
@@ -34,7 +33,6 @@ then
     then
         download_suffix=$file_suffix".gz"
     fi
-
     #always download forward reads
     aws s3 cp $input_address/$fastq_end1$download_suffix $workspace/
     gunzip -q $workspace/$fastq_end1$download_suffix
@@ -49,36 +47,22 @@ fi
 ##END_DOWNLOAD##
 
 
-##BOWTIE 2 ALIGNMENT##
-
-# go to the directory of the index file
-cd $bowtie2_index
-
-# indexing a reference genome (already built, moved under software/bowtie2_index)
-# cd ~
-# $bowtie/bowtie2-build $hairpin_human_fa $basename
-
+##ALIGNMENT##
 if [ "$fastq_end2" == "NULL" ]
 then
-    # single end
-    $bowtie2/bowtie2 -x $basename -U $workspace/$fastq_end1$file_suffix \
-    -S $workspace/$fastq_end1.sam
+    $kallisto/kallisto quant -i $kallisto/kallisto_index -o $workspace/ -t $num_threads --single -l 50 -s 20 \
+    $workspace/$fastq_end1$file_suffix
 else
-    # paired end: using the same output file name ($fastq_end1.sam)
-    $bowtie2/bowtie2 -x $basename -1 $workspace/$fastq_end1$file_suffix \
-    -2 $workspace/$fastq_end2/$file_suffix -S $workspace/$fastq_end1.sam
+    $kallisto/kallisto quant -i $kallisto/kallisto_index -o $workspace/ -t $num_threads -l 50 \
+    $workspace/$fastq_end1$file_suffix $workspace/$fastq_end2$file_suffix
 fi
+##END_ALIGNMENT##
 
-##END BOWTIE 2 ##
-
-# Produce text files to workspace, for multiqc analysis
-$samtools stats $workspace/$fastq_end1.sam > $workspace/$fastq_end1.txt
-echo "Finished samtools stats"
+## Rename files ##
+mv $workspace/abundance.h5 $workspace/$fastq_end1.abundance.h5
+mv $workspace/abundance.tsv $workspace/$fastq_end1.abundance.tsv
+mv $workspace/run_info.json $workspace/$fastq_end1.run_info.json
 
 ##UPLOAD##
-aws s3 cp $workspace $output_address --exclude "*" --include "*.sam*" --recursive
-
-# upload the txt files from samtool stats
-aws s3 cp $workspace $output_address --exclude "*" --include "*.txt*" --recursive
+aws s3 cp $workspace $output_address/ --exclude "*" --include "*.json*" --include "*.h5*" --include "*.tsv*" --recursive
 ##END_UPLOAD##
-
