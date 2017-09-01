ca235abdb97207fe644cc5c7c3fec81a3cd6667e
mustafa-guler <mguler@ucsd.edu>
2017-09-01 14:23:28 -0700

    more tests

---
+++ test/test_cirrus_ngs/test_Pipelines/test_Pipeline.py
@@ -0,0 +1,168 @@
+import unittest
+import sys
+import os
+sys.path.append("../src/cirrus_ngs/server/Pipelines/")
+import Pipeline
+
+class Tests(unittest.TestCase):
+    # Next 4 tests test the different combinations of fastq/fq and zipped/unzipped extensions
+    # _separate_file_suffix should be able to correctly handle any of these combinations
+    def test_separate_file_suffix_unzipped_fastq(self):
+        sample_file = "Sample1_R1.fastq"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1")
+        self.assertEqual(file_suffix, ".fastq")
+        self.assertEqual(is_zipped, "False")
+
+        sample_file = "Sample1_R1.trim.garbage.bye.fastq"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1.trim.garbage.bye")
+        self.assertEqual(file_suffix, ".fastq")
+        self.assertEqual(is_zipped, "False")
+
+        sample_file = "Sample1_R1.trim.garbage.fastq.bye"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1.trim.garbage")
+        self.assertEqual(file_suffix, ".fastq.bye")
+        self.assertEqual(is_zipped, "False")
+
+    def test_separate_file_suffix_zipped_fastq(self):
+        sample_file = "Sample1_R1.fastq.gz"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1")
+        self.assertEqual(file_suffix, ".fastq")
+        self.assertEqual(is_zipped, "True")
+
+        sample_file = "Sample1_R1.trim.garbage.bye.fastq.gz"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1.trim.garbage.bye")
+        self.assertEqual(file_suffix, ".fastq")
+        self.assertEqual(is_zipped, "True")
+
+        sample_file = "Sample1_R1.trim.garbage.fastq.gz.bye"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1.trim.garbage")
+        self.assertEqual(file_suffix, ".fastq.bye")
+        self.assertEqual(is_zipped, "True")
+
+    def test_separate_file_suffix_unzipped_fq(self):
+        sample_file = "Sample1_R1.fq"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1")
+        self.assertEqual(file_suffix, ".fq")
+        self.assertEqual(is_zipped, "False")
+
+        sample_file = "Sample1_R1.trim.garbage.bye.fq"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1.trim.garbage.bye")
+        self.assertEqual(file_suffix, ".fq")
+        self.assertEqual(is_zipped, "False")
+
+        sample_file = "Sample1_R1.trim.garbage.fq.bye"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1.trim.garbage")
+        self.assertEqual(file_suffix, ".fq.bye")
+        self.assertEqual(is_zipped, "False")
+
+    def test_separate_file_suffix_zipped_fq(self):
+        sample_file = "Sample1_R1.fq.gz"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1")
+        self.assertEqual(file_suffix, ".fq")
+        self.assertEqual(is_zipped, "True")
+
+        sample_file = "Sample1_R1.trim.garbage.bye.fq.gz"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1.trim.garbage.bye")
+        self.assertEqual(file_suffix, ".fq")
+        self.assertEqual(is_zipped, "True")
+
+        sample_file = "Sample1_R1.trim.garbage.fq.gz.bye"
+        file_prefix, file_suffix, is_zipped = Pipeline._separate_file_suffix(sample_file)
+        self.assertEqual(file_prefix, "Sample1_R1.trim.garbage")
+        self.assertEqual(file_suffix, ".fq.bye")
+        self.assertEqual(is_zipped, "True")
+
+    def test_by_all_samples_argument_generator(self):
+        sample_list = [ {   "filename": "sample1_forward-fq,sample1_backward.fq",
+                            "description": "sample1_forward",
+                            "group": "groupA"   },
+                        {   "filename": "sample2.fq",
+                            "description": "sample2",
+                            "group": "groupB"   }   ]
+
+        config_dict = { "script_name": "test",
+                        "download_suffix": None,
+                        "input_is_output": False,
+                        "can_be_zipped": True,
+                        "uses_chromosomes": False   }
+        self.fail()
+
+    def test_sample_argument_generator(self):
+        sample_list = [ {   "filename": "sample1_forward.fq,sample1_backward.fq",
+                            "description": "sample1_forward",
+                            "group": "groupA"   },
+                        {   "filename": "sample2.fq.gz",
+                            "description": "sample2",
+                            "group": "groupB"   }   ]
+
+        config_dicts = [{"script_name": "test",
+                        "download_suffix": None,
+                        "input_is_output": False,
+                        "can_be_zipped": True,
+                        "uses_chromosomes": False},
+                        {"script_name": "test",
+                        "download_suffix": ".ext{}.ext2",
+                        "input_is_output": False,
+                        "can_be_zipped": False,
+                        "uses_chromosomes": False},
+                        {"script_name": "test",
+                        "download_suffix": ".ext{}.ext2",
+                        "input_is_output": True,
+                        "can_be_zipped": False,
+                        "uses_chromosomes": True}]
+
+        base_correct_result = "test_proj {} /scratch {} {} {} {} /path/to/logs {}"
+        curr_correct_result = base_correct_result
+
+
+        result1 = [curr_correct_result.format(".fq", "sample1_forward", "sample1_backward", "s3://path/to/input", "s3://path/to/output/test_proj/sample1_forward", "False").split() ]
+        curr_correct_result = base_correct_result
+        result1.append(curr_correct_result.format(".fq", "sample2", "NULL", "s3://path/to/input", "s3://path/to/output/test_proj/sample2", "True").split() )
+        curr_correct_result = base_correct_result
+
+        result2 = [curr_correct_result.format(".ext.fq.ext2", "sample1_forward", "sample1_backward", "s3://path/to/input", "s3://path/to/output/test_proj/sample1_forward", "False").split() ] 
+        curr_correct_result = base_correct_result
+        result2.append(curr_correct_result.format(".ext.fq.ext2", "sample2", "NULL", "s3://path/to/input", "s3://path/to/output/test_proj/sample2", "False").split() )
+        curr_correct_result = base_correct_result
+
+        result3 = [curr_correct_result.format(".ext{}.ext2", "sample1_forward", "sample1_backward", "s3://path/to/output/test_proj/sample1_forward",  "s3://path/to/output/test_proj/sample1_forward", "False").split() ] 
+        curr_correct_result = base_correct_result
+        result3.append(curr_correct_result.format(".ext{}.ext2", "sample2", "NULL", "s3://path/to/output/test_proj/sample2",  "s3://path/to/output/test_proj/sample2", "False").split() )
+
+
+        correct_results = [result1, result2, result3]
+        for index, config in enumerate(config_dicts):
+            result = []
+            for output in Pipeline._sample_argument_generator("test_proj",
+                    sample_list, "s3://path/to/input", "s3://path/to/output",
+                    config, "/path/to/logs"):
+                result.append(output)
+
+            self.assertEquals(result, correct_results[index])
+
+    def test_by_group_argument_generator(self):
+        group_list = []
+        self.fail()
+
+
+
+        
+        
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
