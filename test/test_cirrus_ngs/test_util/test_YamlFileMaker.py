fa14540e02b61dc2dca54a17048c317c7fcb7af1
mustafa-guler <mguler@ucsd.edu>
2017-09-05 13:40:20 -0700

    updated yaml and pipeline unit tests

--- test/test_cirrus_ngs/test_util/test_YamlFileMaker.py
+++ test/test_cirrus_ngs/test_util/test_YamlFileMaker.py
@@ -7,7 +7,8 @@ import tempfile
 import time
 
 class Tests(unittest.TestCase):
-    def test_make_yaml_file_proper_number_of_lines(self):
+
+    def test_make_yaml_file_single_end_no_pairs_list(self):
         yaml_file = tempfile.NamedTemporaryFile("w", delete=False)
         yaml_file.close()
 
@@ -16,144 +17,183 @@ class Tests(unittest.TestCase):
         s3_input_files_address = "s3://fake/s3/path"
         sample_list = [["sample1.fq"], ["sample2.fq"]]
         group_list = ["groupA", "groupB"]
+        pair_list = None
         s3_output_files_address = "s3://fake/s3/out/path"
-        genome = "NA"
-        style = "NA"
+        genome = "hg19"
+        style = "histone"
 
         yfm.make_yaml_file(yaml_file.name, project_name, analysis_steps,
                 s3_input_files_address, sample_list, group_list,
-                s3_output_files_address, genome, style)
+                s3_output_files_address, genome, style, None)
 
-        temp = open(yaml_file.name, "r")
-        yaml_file_contents = temp.read().split("\n")
-        temp.close()
+        date = time.strftime("%Y/%m/%d")
 
-        self.assertEqual(len(yaml_file_contents), 18)
-
-    def test_make_yaml_file_single_end_sample_output(self):
-        yaml_file = tempfile.NamedTemporaryFile("w", delete=False)
-        yaml_file.close()
-
-        project_name = "test_project"
-        analysis_steps = ["fastqc", "trim", "bwa"]
-        s3_input_files_address = "s3://fake/s3/path"
-        sample_list = [["sample1.fq"], ["sample2.fq"]]
-        group_list = ["groupA", "groupB"]
-        s3_output_files_address = "s3://fake/s3/out/path"
-        genome = "NA"
-        style = "NA"
-
-        yfm.make_yaml_file(yaml_file.name, project_name, analysis_steps,
-                s3_input_files_address, sample_list, group_list,
-                s3_output_files_address, genome, style)
+        correct_output = """project: test_project
+analysis:
+  - fastqc
+  - trim
+  - bwa
+date: """ + date +"""
+upload: s3://fake/s3/out/path
+download: s3://fake/s3/path
+genome: hg19
+style: histone
+pairs:
+  {}
+sample:
+- filename: sample1.fq
+  description: sample1
+  group: groupA
+- filename: sample2.fq
+  description: sample2
+  group: groupB
+"""
 
         temp = open(yaml_file.name, "r")
         yaml_file_contents = temp.read()
         temp.close()
 
-        date = time.strftime("%Y/%m/%d")
-        correct_output = """project: test_project
-analysis: fastqc, trim, bwa
-date: {}
-upload: s3://fake/s3/out/path
-genome: NA
-style: NA
----
-sample:
-  filename: sample1.fq
-  download: s3://fake/s3/path
-  description: sample1
-  group: groupA
----
-sample:
-  filename: sample2.fq
-  download: s3://fake/s3/path
-  description: sample2
-  group: groupB""".format(date)
+        self.assertEqual(yaml_file_contents, correct_output)
 
-        self.assertEqual(correct_output, yaml_file_contents)
-
-    def test_make_yaml_file_paired_end_sample_output(self):
+    def test_make_yaml_file_single_end_pairs_list(self):
         yaml_file = tempfile.NamedTemporaryFile("w", delete=False)
         yaml_file.close()
 
         project_name = "test_project"
-        analysis_steps = ["fastqc", "trim", "bwa"]
+        analysis_steps = ["trim", "bwa"]
+        s3_input_files_address = "s3://fake/s3/path"
+        sample_list = [["sample1.fq"], ["sample1b.fq"], ["sample2.fq"], ["sample2b.fq"]]
+        group_list = ["groupA", "groupA", "groupB", "groupB"]
+        pair_list = { "sample1": "sample1b", "sample2": "sample2b" }
+        s3_output_files_address = "s3://fake/s3/out/path"
+        genome = "hg19"
+        style = "histone"
+
+        yfm.make_yaml_file(yaml_file.name, project_name, analysis_steps,
+                s3_input_files_address, sample_list, group_list,
+                s3_output_files_address, genome, style, pair_list)
+
+        date = time.strftime("%Y/%m/%d")
+
+        correct_output = """project: test_project
+analysis:
+  - trim
+  - bwa
+date: """ + date +"""
+upload: s3://fake/s3/out/path
+download: s3://fake/s3/path
+genome: hg19
+style: histone
+pairs:
+  sample1: sample1b
+  sample2: sample2b
+sample:
+- filename: sample1.fq
+  description: sample1
+  group: groupA
+- filename: sample1b.fq
+  description: sample1b
+  group: groupA
+- filename: sample2.fq
+  description: sample2
+  group: groupB
+- filename: sample2b.fq
+  description: sample2b
+  group: groupB
+"""
+        temp = open(yaml_file.name, "r")
+        yaml_file_contents = temp.read()
+        temp.close()
+
+        self.assertEqual(yaml_file_contents, correct_output)
+
+    def test_make_yaml_file_paired_end_no_pairs_list(self):
+        yaml_file = tempfile.NamedTemporaryFile("w", delete=False)
+        yaml_file.close()
+
+        project_name = "test_project"
+        analysis_steps = ["bwa"]
         s3_input_files_address = "s3://fake/s3/path"
         sample_list = [["sample1.fq", "reverse1.fq"], ["sample2.fq", "reverse2.fq"]]
         group_list = ["groupA", "groupB"]
+        pair_list = None
         s3_output_files_address = "s3://fake/s3/out/path"
-        genome = "NA"
-        style = "NA"
+        genome = "hg19"
+        style = "histone"
 
         yfm.make_yaml_file(yaml_file.name, project_name, analysis_steps,
                 s3_input_files_address, sample_list, group_list,
-                s3_output_files_address, genome, style)
+                s3_output_files_address, genome, style, pair_list)
 
+        date = time.strftime("%Y/%m/%d")
+
+        correct_output = """project: test_project
+analysis:
+  - bwa
+date: """ + date +"""
+upload: s3://fake/s3/out/path
+download: s3://fake/s3/path
+genome: hg19
+style: histone
+pairs:
+  {}
+sample:
+- filename: sample1.fq, reverse1.fq
+  description: sample1
+  group: groupA
+- filename: sample2.fq, reverse2.fq
+  description: sample2
+  group: groupB
+"""
         temp = open(yaml_file.name, "r")
         yaml_file_contents = temp.read()
         temp.close()
 
-        date = time.strftime("%Y/%m/%d")
-        correct_output = """project: test_project
-analysis: fastqc, trim, bwa
-date: {}
-upload: s3://fake/s3/out/path
-genome: NA
-style: NA
----
-sample:
-  filename: sample1.fq, reverse1.fq
-  download: s3://fake/s3/path
-  description: sample1
-  group: groupA
----
-sample:
-  filename: sample2.fq, reverse2.fq
-  download: s3://fake/s3/path
-  description: sample2
-  group: groupB""".format(date)
+        self.assertEqual(yaml_file_contents, correct_output)
 
-        self.assertEqual(correct_output, yaml_file_contents)
-
-    def test_make_yaml_file_proper_indentation(self):
+    def test_make_yaml_file_paired_end_pairs_list(self):
         yaml_file = tempfile.NamedTemporaryFile("w", delete=False)
         yaml_file.close()
 
         project_name = "test_project"
-        analysis_steps = ["fastqc", "trim", "bwa"]
+        analysis_steps = ["bwa"]
         s3_input_files_address = "s3://fake/s3/path"
-        sample_list = [["sample1.fq"], ["sample2.fq"]]
+        sample_list = [["sample1.fq", "reverse1.fq"], ["sample2.fq", "reverse2.fq"]]
         group_list = ["groupA", "groupB"]
+        pair_list = {"sample1": "sample2"}
         s3_output_files_address = "s3://fake/s3/out/path"
-        genome = "NA"
-        style = "NA"
+        genome = "hg19"
+        style = "histone"
 
         yfm.make_yaml_file(yaml_file.name, project_name, analysis_steps,
                 s3_input_files_address, sample_list, group_list,
-                s3_output_files_address, genome, style)
+                s3_output_files_address, genome, style, pair_list)
 
+        date = time.strftime("%Y/%m/%d")
+
+        correct_output = """project: test_project
+analysis:
+  - bwa
+date: """ + date +"""
+upload: s3://fake/s3/out/path
+download: s3://fake/s3/path
+genome: hg19
+style: histone
+pairs:
+  sample1: sample2
+sample:
+- filename: sample1.fq, reverse1.fq
+  description: sample1
+  group: groupA
+- filename: sample2.fq, reverse2.fq
+  description: sample2
+  group: groupB
+"""
         temp = open(yaml_file.name, "r")
-        indented_lines = temp.read().split("---\nsample:\n")[1:]
+        yaml_file_contents = temp.read()
         temp.close()
 
-        for line in indented_lines:
-            self.assertEqual(line[:2], "  ")
-
-
-        sample_list = [["sample1.fq", "reverse"], ["sample2.fq", "reverse2"], ["sample3.fq"], ["sampe4.fq"]]
-        group_list = ["groupA", "groupB", "groupA", "groupB"]
-
-        yfm.make_yaml_file(yaml_file.name, project_name, analysis_steps,
-                s3_input_files_address, sample_list, group_list,
-                s3_output_files_address, genome, style)
-
-        temp = open(yaml_file.name, "r")
-        indented_lines = temp.read().split("---\nsample:\n")[1:]
-        temp.close()
-
-        for line in indented_lines:
-            self.assertEqual(line[:2], "  ")
+        self.assertEqual(yaml_file_contents, correct_output)
 
 if __name__ == "__main__":
     unittest.main()
