a9d1b95c5f4868764d8ff474e44aa90820065394
mustafa-guler <mguler@ucsd.edu>
2017-09-06 10:32:29 -0700

    workflow dirs for yaml files

--- src/cirrus_ngs/server/Pipelines/scripts/DNASeq/post.sh
+++ src/cirrus_ngs/server/Pipelines/scripts/DNASeq/post.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 project_name=$1
-file_suffix=$2  #extension of input file, does not include .gz if present in input
-root_dir=$3
-fastq_end1=$4
-fastq_end2=$5
-input_address=$6    #this is an s3 address e.g. s3://path/to/input/directory
-output_address=$7   #this is an s3 address e.g. s3://path/to/output/directory
-log_dir=$8
-is_zipped=$9    #either "True" or "False", indicates whether input is gzipped
-numb_threads=${10}
-chromosome=${11}
+workflow=$2
+file_suffix=$3  #extension of input file, does not include .gz if present in input
+root_dir=$4
+fastq_end1=$5
+fastq_end2=$6
+input_address=$7    #this is an s3 address e.g. s3://path/to/input/directory
+output_address=$8   #this is an s3 address e.g. s3://path/to/output/directory
+log_dir=$9
+is_zipped=${10}    #either "True" or "False", indicates whether input is gzipped
+numb_threads=${11}
+chromosome=${12}
 
 #logging
 log_dir=$log_dir/$fastq_end1
