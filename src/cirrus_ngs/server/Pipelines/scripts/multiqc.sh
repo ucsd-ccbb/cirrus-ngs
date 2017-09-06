adb7821307c6ea0cf24d062990fe72a1d559aacf
Mengyi Liu <mikoliu798@gmail.com>
2017-09-06 10:37:10 -0700

    Edited shared shell scripts

--- src/cirrus_ngs/server/Pipelines/scripts/multiqc.sh
+++ src/cirrus_ngs/server/Pipelines/scripts/multiqc.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 project_name=$1
-file_suffix=$2  # not used, for shell script convention
-root_dir=$3
-fastq_end1=$4    # always NULL, for shell script convention
-fastq_end2=$5    # always NULL, for shell script convention
-input_address=$6    # this is an s3 address e.g. s3://path/to/input/directory
-output_address=$7   # this is an s3 address e.g. s3://path/to/output/directory
-log_dir=$8
-is_zipped=$9    # not used, for shell script convention
-all_samples=${10}
+workflow=$2
+file_suffix=$3  #extension of input file, does not include .gz if present in input
+root_dir=$4
+fastq_end1=$5
+fastq_end2=$6
+input_address=$7    #this is an s3 address e.g. s3://path/to/input/directory
+output_address=$8   #this is an s3 address e.g. s3://path/to/output/directory
+log_dir=$9
+is_zipped=$10    #either "True" or "False", indicates whether input is gzipped
+all_samples=${11}
 
 mkdir -p $log_dir
 log_file=$log_dir/'multiqc.log'
@@ -19,22 +20,19 @@ exec 2>>$log_file
 # set workspace to the multiqc directory so that multiqc would search for files
 # in this directory
 workspace=$root_dir/$project_name/multiqc
-software=/shared/workspace/software
-multiqc=$software/anaconda3/bin/multiqc
-fastqc=_fastqc.zip
-stats=.stats.txt
-
 mkdir -p $workspace
 
+# Download fastqc zip files and text files from alignment
 for file in $all_samples; do
-    aws s3 cp $input_address/$file/$file$fastqc $workspace/
-    aws s3 cp $input_address/$file/$file$stats $workspace/
+    aws s3 cp $input_address/$file/ $workspace/ --exclude "*" --include "*_fastqc.zip" --recursive
+    aws s3 cp $input_address/$file/ $workspace/ --exclude "*" --include "*.txt" --recursive
+
 done
 
-
-# Multiqc on all:
+# Multiqc on all
+#       .fastqc.zip files
 #       .txt files from alignment
-#       fastqc.zip files
+
 $multiqc -f $workspace -o $workspace
 
 # Upload the html file to s3
