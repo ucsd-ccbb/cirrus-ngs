b2b96670350fe341c2089d7c46a130427c37335c
Mengyi Liu <mikoliu798@gmail.com>
2017-09-06 10:37:10 -0700

    Edited shared shell scripts

--- src/cirrus_ngs/server/Pipelines/scripts/fastqc.sh
+++ src/cirrus_ngs/server/Pipelines/scripts/fastqc.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 project_name=$1
-file_suffix=$2  #extension of input file, does not include .gz if present in input
-root_dir=$3
-fastq_end1=$4
-fastq_end2=$5
-input_address=$6    #this is an s3 address e.g. s3://path/to/input/directory
-output_address=$7   #this is an s3 address e.g. s3://path/to/output/directory
-log_dir=$8
-is_zipped=$9    #either "True" or "False", indicates whether input is gzipped
+workflow=$2
+file_suffix=$3  #extension of input file, does not include .gz if present in input
+root_dir=$4
+fastq_end1=$5
+fastq_end2=$6
+input_address=$7    #this is an s3 address e.g. s3://path/to/input/directory
+output_address=$8   #this is an s3 address e.g. s3://path/to/output/directory
+log_dir=$9
+is_zipped=$10    #either "True" or "False", indicates whether input is gzipped
 
 #logging
 log_dir=$log_dir/$fastq_end1
@@ -21,7 +22,7 @@ status_file=$log_dir/'status.log'
 touch $status_file
 
 #prepare output directories
-workspace=$root_dir/$project_name/$fastq_end1
+workspace=$root_dir/$project_name/$workflow/$fastq_end1
 mkdir -p $workspace
 
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
