266b0426c4c7395e6b39788f47a6557335d2c90e
Mengyi Liu <mikoliu798@gmail.com>
2017-09-07 16:48:13 -0700

    updated shell scripts in RNASeq

--- src/cirrus_ngs/server/Pipelines/scripts/RNASeq/make_group.sh
+++ src/cirrus_ngs/server/Pipelines/scripts/RNASeq/make_group.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 project_name=$1
-file_suffix=$2  #extension of input file, does not include .gz if present in input
-root_dir=$3
-fastq_end1=$4
-fastq_end2=$5
-input_address=$6    #this is an s3 address e.g. s3://path/to/input/directory
-output_address=$7   #this is an s3 address e.g. s3://path/to/output/directory
-log_dir=$8
-is_zipped=$9    #either "True" or "False", indicates whether input is gzipped
+workflow=$2
+file_suffix=$3  #extension of input file, does not include .gz if present in input
+root_dir=$4
+fastq_end1=$5
+fastq_end2=$6
+input_address=$7    #this is an s3 address e.g. s3://path/to/input/directory
+output_address=$8   #this is an s3 address e.g. s3://path/to/output/directory
+log_dir=$9
+is_zipped=${10}    #either "True" or "False", indicates whether input is gzipped
 
 #logging
 mkdir -p $log_dir
@@ -16,9 +17,18 @@ log_file=$log_dir/'make_group.log'
 exec 1>>$log_file
 exec 2>>$log_file
 
+status_file=$log_dir/'status.log'
+touch $status_file
+
 #prepare output directories
-workspace=$root_dir/$project_name
+workspace=$root_dir/$project_name/$workflow
 mkdir -p $workspace
+
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+date
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+check_step_already_done $JOB_NAME $status_file
 
 # Make a new group file
 touch $workspace/group.txt
