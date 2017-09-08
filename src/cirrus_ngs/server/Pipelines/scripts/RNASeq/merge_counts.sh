474c3f04b40e0268dc40e56269ac33a02b3b1b25
Mengyi Liu <mikoliu798@gmail.com>
2017-09-08 10:05:37 -0700

    updated shell scripts in RNASeq_star_htseq

--- src/cirrus_ngs/server/Pipelines/scripts/RNASeq/merge_counts.sh
+++ src/cirrus_ngs/server/Pipelines/scripts/RNASeq/merge_counts.sh
@@ -41,7 +41,8 @@ for file in $all_samples; do
 done
 
 # Call the merge count file
-python /shared/workspace/Pipelines/util/MergeCountFile.py $workflow "$all_samples" $workspace
+check_exit_status "python /shared/workspace/Pipelines/util/MergeCountFile.py $workflow $workspace $all_samples" $JOB_NAME $status_file
 
 # Upload the output file
-check_exit_status "aws s3 cp $workspace $output_address/ --exclude "*" --include "*all_gene_counts.txt*" --recursive" $JOB_NAME $status_file
+check_exit_status "aws s3 cp $workspace $output_address/ --exclude "*" --include "*all_gene_counts.txt*" \
+--recursive" $JOB_NAME $status_file
