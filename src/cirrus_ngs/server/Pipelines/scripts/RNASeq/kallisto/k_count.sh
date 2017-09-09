756c525ea38450585e4cc11f7ea6400cf69f1b0c
Mengyi Liu <mikoliu798@gmail.com>
2017-09-08 17:04:31 -0700

    updated shell scripts and python files in RNA Seq

--- src/cirrus_ngs/server/Pipelines/scripts/RNASeq/kallisto/k_count.sh
+++ src/cirrus_ngs/server/Pipelines/scripts/RNASeq/kallisto/k_count.sh
@@ -1,28 +1,62 @@
 #!/bin/bash
 
 project_name=$1
-file_suffix=$2
-root_dir=$3
-fastq_end1=$4
-fastq_end2=$5
-input_address=$6    #this is an s3 address e.g. s3://path/to/input/directory
-output_address=$7   #this is an s3 address e.g. s3://path/to/output/directory
-log_dir=$8
-is_zipped=$9
+workflow=$2
+file_suffix=$3  #extension of input file, does not include .gz if present in input
+root_dir=$4
+fastq_end1=$5
+fastq_end2=$6
+input_address=$7    #this is an s3 address e.g. s3://path/to/input/directory
+output_address=$8   #this is an s3 address e.g. s3://path/to/output/directory
+log_dir=$9
+is_zipped=${10}    #either "True" or "False", indicates whether input is gzipped
 
 #logging
-mkdir -p $log_dir/$fastq_end1
-log_file=$log_dir/$fastq_end1/'k_count.log'
+log_dir=$log_dir/$fastq_end1
+mkdir -p $log_dir
+log_file=$log_dir/'k_count.log'
 exec 1>>$log_file
 exec 2>>$log_file
 
 echo $output_address
 
+status_file=$log_dir/'status.log'
+touch $status_file
+
+#prepare output directories
+workspace=$root_dir/$project_name/$workflow/$fastq_end1
+mkdir -p $workspace
+
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+date
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+check_step_already_done $JOB_NAME $status_file
+
 # sample=$1
 # output_file=$2
 
+# Download abundance text file
+if [ ! -f $workspace/$fastq_end1$file_suffix ]
+then
+    #this is the suffix of the input from s3
+    download_suffix=$file_suffix
+
+    #changes extension if S3 input is zipped
+    if [ "$is_zipped" == "True" ]
+    then
+        download_suffix=$file_suffix".gz"
+    fi
+    aws s3 cp $input_address/$fastq_end1$download_suffix $workspace/
+    gunzip -q $workspace/$fastq_end1$download_suffix
+
+fi
+##END_DOWNLOAD##
+
 # Call the perl file
-perl $kallisto_counts $output_address
+perl $kallisto_counts $workspace/$fastq_end1 $output_address
 
-
+##UPLOAD##
+aws s3 cp $workspace $output_address/ --exclude "*" --include "*_counts.txt*" --recursive
+##END_UPLOAD##
 
--- src/cirrus_ngs/server/Pipelines/util/GroupFileMaker.py
+++ src/cirrus_ngs/server/Pipelines/util/GroupFileMaker.py
@@ -2,21 +2,17 @@ __author__ = 'Guorong Xu<g1xu@ucsd.edu>'
 
 import sys
 import yaml
-#import YamlFileReader
-
 
 ## make a group file for analysis of the pipeline
 # group_file: path to group file to be generated
 # yaml_file: path to yaml file for the project
 # s3_path: directory for the all_gene_counts file on s3
 def make_group_file(group_file, yaml_file, s3_path):
-    # TODO: deprecate YamlFileReader
 
     yaml_file_stream = open(yaml_file)
     documents = yaml.load(yaml_file_stream)
     yaml_file_stream.close()
 
-    #documents = YamlFileReader.parse_yaml_file(yaml_file)
     sample_list = documents.get("sample")
     print("In GFM, sample list: "+sample_list)
 
@@ -42,7 +38,7 @@ if __name__ == "__main__":
 
     group_file = sys.argv[1]
     yaml_file = sys.argv[2]
-    sample_list = sys.argv[3]
+    s3_path = sys.argv[3]
 
-    make_group_file(group_file, yaml_file, sample_list)
+    make_group_file(group_file, yaml_file, s3_path)
 
