f75129d345a6dce8eda15a5af866bb7edc9bc4f8
mustafa-guler <mguler@ucsd.edu>
2017-11-21 13:02:23 -0800

    added more docstrings, made more doc htmls

--- src/cirrus_ngs/util/PipelineManager.py
+++ src/cirrus_ngs/util/PipelineManager.py
@@ -1,3 +1,9 @@
+__doc__="""
+This module contains the main pipeline execution method on the 
+local side. It is shared between all pipelines and workflows with
+differing parameters.
+"""
+
 import os
 from util import YamlFileMaker
 from cfnCluster import ConnectionManager
@@ -9,6 +15,25 @@ workspace = "/shared/workspace/Pipelines/"
 # executing a specified pipeline with the specific yaml file
 def execute(pipeline, ssh_client, project_name, workflow, analysis_steps, s3_input_files_address,
         sample_list, group_list, s3_output_files_address, genome, style, pairs_list):
+    """
+    The main local side function for executing a pipeline with all user inputs to jupyter notebook.
+    Calls the run.sh shell script on the cluster head node using nohup.
+
+    input:
+        pipeline: name of the pipeline to be run, supported pipelines can be found in CirrusAddons notebook
+        ssh_client: a paramiko SSHClient object that connects to the cluster where analysis is run
+        project_name: name of the current project, <project_name>.yaml contains all user input to notebook
+        workflow: name of the workflow to be run, supported workflows can be found in CirrusAddons notebook
+        analysis_steps: set of analysis steps to be run, supported steps can be found in pipeline's notebook
+        s3_input_files_address: s3 bucket containing all fastq files for project
+        sample_list: list of dictionaries with sample info for each sample
+        group_list: list of all groups, shares indices with sample_list (sample_list[0] is in group[0], etc)
+        s3_output_files_address: root s3 bucket where analysis results should be uploaded
+        genome: reference genome to be used, supported genomes can be found in pipeline's notebook
+        style: only for ChIPSeq homer workflow, can be "factor" or "histone"
+        pairs_list: dictionary with keys=normal samples, values=experimental samples
+            for ChIPSeq the keys=ChIP samples, values=corresponding input regularization samples
+    """
     yaml_file = project_name + ".yaml"
 
     if s3_output_files_address.endswith("/"):
@@ -34,9 +59,9 @@ def execute(pipeline, ssh_client, project_name, workflow, analysis_steps, s3_inp
                                       "nohup bash " + workspace + "scripts/run.sh "
                                       + workspace + "yaml_files/{}/{}/{} ".format(pipeline, workflow, yaml_file)
                                       + logs_dir + " " + pipeline+"_"+workflow)
-
-def check_status(ssh_client, job_name):
-    print("checking status of jobs...")
-    qstat = ConnectionManager.execute_command(ssh_client, "qstat")
-    logs = ConnectionManager.list_dir(ssh_client, logs_dir)
-    QstatParser.parse_qstat(qstat, logs, job_name)
+#
+#def check_status(ssh_client, job_name):
+#    print("checking status of jobs...")
+#    qstat = ConnectionManager.execute_command(ssh_client, "qstat")
+#    logs = ConnectionManager.list_dir(ssh_client, logs_dir)
+#    QstatParser.parse_qstat(qstat, logs, job_name)
