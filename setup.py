64e1c4d002f23925766ca17700347c0f64cfe777
mustafa-guler <mguler@ucsd.edu>
2018-04-22 23:45:45 -0700

    packaging start

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "cirrus-ngs",
+    version = "1.0.0a1",
+    description = "Cloud-optimized primary analysis pipelines for RNA-seq, \
+            miRNA-seq, ChIP-seq, and variant calling in \
+            whole-genome/whole-exome DNA-seq",
+    url = "https://github.com/ucsd-ccbb/cirrus-ngs",
+    license = "MIT",
+    author = "ucsd-ccbb",
+    author_email = "mguler@ucsd.edu",
+    classifiers = 
+    [
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+    keywords = "bioinformatics, NGS, jupyter",
+    project_urls = 
+    {
+        "Source": "https://github.com/ucsd-ccbb/cirrus-ngs",
+        "Tracker": "https://github.com/ucsd-ccbb/cirrus-ngs/issues"
+    },
+    install_requires = [""]
+)
+
+
+
